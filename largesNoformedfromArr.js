8f31636de985ca42397468b83febf9a05e1e241d
Arpit Garg <garg.arpit504@gmail.com>
2019-05-12 00:29:27 +0530

    Update largesNoformedfromArr.js

--- largesNoformedfromArr.js
+++ largesNoformedfromArr.js
@@ -1,28 +1,24 @@
 
 // https://www.geeksforgeeks.org/given-an-array-of-numbers-arrange-the-numbers-to-form-the-biggest-number/
 // example {1, 34, 3, 98, 9, 76, 45, 4} 
-//  (34,3) (1) (98,9) (76) (45,4)
+
+function arrange(array){
    
-function (array){
- // create bucket based on MSB of each no
- // concat and resolve 
- let bucket = {};
-  for(let i=0;i<array.length;i++){
-       let buckKey = getKey(array[i]);
-       bucket[buckKey].push(array[i]);
-  }
-  
-  //
-  
+   var arr2 = array.sort(function(x,y){
+     return giveMax(x,y);      
+   });
+   for(var h=0;h<arr2.length;h++){
+      console.log(arr2[h]);
+   
+   }
   
 } 
 
 function giveMax(a,b){ 
  var x= String(a) +String(b);
  var y = String(b) +String(a);
- return Number(x) >= Number(y) ? Number(x):Number(y);
+ return Number(x) >= Number(y) ? -1:1;
 }
 
-function getKey(element){
-    return String(element).substring(0,1);
-  
+arrange([1, 34, 3, 98, 9, 76, 45, 4]);
+
