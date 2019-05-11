44f9354d086f441c4611444ab09ba487c11d1a59
Arpit Garg <garg.arpit504@gmail.com>
2019-05-12 01:06:05 +0530

    Update max_min_alternate_array.js
    https://www.geeksforgeeks.org/rearrange-array-maximum-minimum-form/

--- max_min_alternate_array.js
+++ max_min_alternate_array.js
@@ -1,1 +1,25 @@
 
+function alternateMaxMin(array){
+  var soln = [];
+  var i=0;
+  var j = array.length - 1;
+  var mid = Math.ceil(j/2);
+  
+  if(array.length %2 == 0){
+      while(i<j){
+        soln.push(array[j--]);
+        soln.push(array[i++]);
+      }
+  }else{
+      while(i !==j){
+         soln.push(array[j--]);
+        soln.push(array[i++]);
+      }
+      soln.push(array[i]);
+  }  
+  return soln.join();
+  
+}
+
+alternateMaxMin([1,2,3,4,5,6,7,8 ])
+ alternateMaxMin([1,2,3,4,5,6,7 ]) 
