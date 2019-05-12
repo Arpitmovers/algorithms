2f50f9b25753327a0b1a190ea379c49006f5954c
Arpit Garg <garg.arpit504@gmail.com>
2019-05-12 17:31:35 +0530

    Create segregateOddEven.js
    https://practice.geeksforgeeks.org/problems/segregate-even-and-odd-numbers/0/?ref=self

---
+++ segregateOddEven.js
@@ -0,0 +1,8 @@
+// given an aray of nos , segregate all even at start and odd at end
+
+Soln:
+-start from 0 , length-1
+-if a[i] is even i++
+-if a[j] is odd j--
+-swap a[i],a[j] if a[i] is odd , a[j] is even
+
