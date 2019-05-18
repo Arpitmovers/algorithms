4c89eed00b90276d8792ecef78e2ac54f4b3e818
Arpit Garg <garg.arpit504@gmail.com>
2019-05-18 15:42:52 +0530

    Update countZerosinSortedArray.js

--- countZerosinSortedArray.js
+++ countZerosinSortedArray.js
@@ -2,20 +2,23 @@
 // solution using binary search as the array is of form [1,1,1,1,0,0]
 function countZero(array, start, end) {
 
-    var mid = Math.floor((start + end) / 2);
-    if ((array[mid] == 0 && array[mid - 1] == 1) || (array[mid] == 1 && array[mid + 1] == 0)) {
-        return mid;
-    }
+    if (end >= start) {
 
-    if (array[mid] == 1) {
-      return  countZero(array, mid + 1, end);
-    }
-    if (array[mid] == 0) {
-       return countZero(array, start, mid);
-    }
 
+        var mid = Math.floor((start + end) / 2);
+        if ((array[mid] == 0 && array[mid - 1] == 1) || (mid==0 && arr[mid] ==0 )) {  // startinng elem is 0 itself
+            return mid;
+        }
+
+        if (array[mid] == 1) {
+            return countZero(array, mid + 1, end);
+        }
+        else
+            return countZero(array, start, mid-1);
+
+    }
+    return -1;
 }
-
 
 var arr = [1, 1, 1, 1, 0, 0];
 var indexZero = countZero(arr, 0, arr.length - 1);
