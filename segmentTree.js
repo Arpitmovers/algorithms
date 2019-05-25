e480a002d6f6c684007ad60482f775e0b95ca17d
Arpit Garg <garg.arpit504@gmail.com>
2019-05-25 20:19:47 +0530

    Update segmentTree.js

--- segmentTree.js
+++ segmentTree.js
@@ -1,45 +1,106 @@
 
 
-function getNodesCount(n) {
-    var height = Math.ceil(Math.log2(n));
-    var totalnodes;
-    totalnodes = 2 * Math.pow(2, height) - 1;
-    return totalnodes;
-
-}
 
 // creates segmentTree for inputElements in range of(startIdx, endIdx)
 // currentIndex is  index in segmentTree (where we have to store data)
-function createSegRecursive(inputElements, startIdx, endIdx, segmentTree, treeIndex) {
+// treeIndex is root of segment tree
+function createSegRecursive(inputElements, startIdx, endIdx, segmentTree, treeIndex, casee) {
+    console.log('startIdx :', startIdx, 'endIdx ', endIdx, 'treeIndex', treeIndex, 'case  ', casee);
 
+    if (treeIndex > segmentTree.length) {
+        return 0;
+    }
     // base case (single element)
     if (startIdx == endIdx) {
         segmentTree[treeIndex] = inputElements[startIdx];
+        console.log('value at ', treeIndex, 'is ', segmentTree[treeIndex]);
         return;
     }
 
     var mid = getMidIndex(startIdx, endIdx);
     // left tree ko build karke la
-    createSegRecursive(inputElements, startIdx, mid, segmentTree, (2 * treeIndex) + 1);
-    // right tree ko build karke la
-    createSegRecursive(inputElements, mid + 1, endIdx, segmentTree, (2 * treeIndex) + 2);
-    segmentTree[treeIndex] = treeIndex[(2 * treeIndex) + 1] + treeIndex[(2 * treeIndex) + 2];
-
+    createSegRecursive(inputElements, startIdx, mid, segmentTree, (treeIndex * 2) + 1, 'left');
+    createSegRecursive(inputElements, mid + 1, endIdx, segmentTree, (treeIndex * 2) + 2, 'right');
+    segmentTree[treeIndex] = segmentTree[(treeIndex * 2) + 1] + segmentTree[(treeIndex * 2) + 2];
 
 }
 
 
+function createSegmentTree(elements) {
+    var count = getNodesCount(elements.length);
+    var array = new Array(count);
+
+    /// is tree ko build karke la 
+    createSegRecursive(elements, 0, elements.length - 1, array, 0, 'top');
+    return array;
+}
+
+
+//positon is root if my segMent tree 
+// positon is for storage at correct location
+// low , high are representation of node  range in segment tree
+function recursiveSum(segmentTree, queryStart, queryEnd, low, high, positon) {
+    console.log('low, high, positon ', low, high, positon);
+    
+    
+    // total overlap
+    if (low >= queryStart && high <= queryEnd) {
+        return segmentTree[positon];
+    }
+
+    // no overlap
+    if (low > queryEnd || high < queryStart) {
+        return 0;
+    }
+
+    var mid = getMidIndex(low, high);
+    var s1 = recursiveSum(segmentTree, queryStart, queryEnd, low, mid, (2 * positon) + 1);
+    var s2 = recursiveSum(segmentTree, queryStart, queryEnd, mid + 1, high, (2 * positon) + 2);
+    console.log(' s1 s2 ', s1, s2);
+    return s1 + s2;
+
+}
+
+
+function findSumInRange(segmentTree, queryStart, queryEnd, origArrLength) {
+    if (queryStart > segmentTree.length || queryEnd < 0 || queryEnd < queryStart) {
+        return null;
+    }
+    return recursiveSum(segmentTree, queryStart, queryEnd, 0, origArrLength - 1, 0);
+}
+
+
+function getNodesCount(n) {
+    // (2 raised to h+1) -1
+    var height = Math.ceil(Math.log2(n));
+    var totalnodes;
+    totalnodes = Math.pow(2, height + 1) - 1;
+    return totalnodes;
+
+}
+ 
 function getMidIndex(start, end) {
     return Math.floor(start + (end - start) / 2);
 }
 
-function createSegmentTree(elements) {
-    count = this.getNodesCount(elements.length);
-    var array = new Array(count);
-    /// is tree ko build karke la 
-    createSegRecursive(elements, 0, array.length - 1, array, 0);
-    return array;
-}
-var arr = [1, 3, 5, 7, 9, 11];
 
-console.log(' segment tree:: ', createSegmentTree(arr));
+var arr = [1, 3, 5, 7, 9, 11];
+//var segmentTree = createSegmentTree(arr);
+var segmentTree = [36, 9, 27, 4, 5, 16, 11, 1, 3, null, null, 7, 9, null, null];
+//console.log(' segment tree:: ', createSegmentTree(arr));
+
+
+
+//range queries: find sum
+var queryStart = 1, queryEnd = 2;  // ans 8
+
+var sum = findSumInRange(segmentTree, queryStart, queryEnd, arr.length);
+
+if (!sum) {
+    console.log(' INVALID');
+} else {
+    console.log('sum in range ', queryStart, queryEnd, ' is ', sum);
+}
+// space complexxity O(n) as total 2n-1 nodes
+// time complexity for update   O(log n)
+// https://www.youtube.com/watch?v=ZBHKZF5w4YU&t=1015s  -
