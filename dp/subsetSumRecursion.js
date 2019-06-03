d35d2582e3fcb1127dd9f5f5966e793842f9796f
Arpit Garg <garg.arpit504@gmail.com>
2019-06-03 20:00:52 +0530

    Create subsetSumRecursion.js
    Complexity: O(2^n)

---
+++ dp/subsetSumRecursion.js
@@ -0,0 +1,18 @@
+
+
+function subsetSum(arr, i, X,useCase) {
+    console.log(' i :', i, 'X is ', X,'case ',useCase);
+    if (X < 0) {
+        return false;
+    }
+
+    if (X == 0) {
+        return true;
+    }
+
+    return subsetSum(arr, i + 1, X - arr[i],1) || subsetSum(arr, i + 1, X,2);
+
+
+}
+var res = subsetSum([3, 2, 7, 1], 0, 6,'main')
+console.log(' ans is ', res);
